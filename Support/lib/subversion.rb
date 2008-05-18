4462c2b923c8d89f3674e5b7ef4d120d7fc6c1fe
Luke Daley <ld@ldaley.com>
2008-05-18 11:10:54 +0000

    New module that will be the basis of *all* svn commands.
    At the moment it just provides `Subversion.run()` which takes arguments to pass to the svn binary. If an error occurs, `exit_with_tool_tip` is called with the error text, otherwise the stdout is returned.

---
+++ Support/lib/subversion.rb
@@ -0,0 +1,26 @@
+require ENV['TM_SUPPORT_PATH'] + '/lib/tm_process'
+require ENV['TM_SUPPORT_PATH'] + '/lib/exit_codes'
+
+module Subversion
+  class << self
+
+    def svn
+      ENV['TM_SVN']
+    end
+
+    def run(*cmd, &error_handler)
+      out, err = TextMate::Process.run([svn, cmd].flatten, :sync => true)
+
+      if $? != 0
+        if error_handler.nil?
+          TextMate.exit_show_tool_tip err
+        else
+          error_handler.call(err)
+        end
+      else
+        return out
+      end
+    end
+
+  end
+end
