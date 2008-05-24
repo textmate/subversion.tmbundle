6dea85c40dbecdc576764fa83a14ad2e51b961ed
Luke Daley <ld@ldaley.com>
2008-05-24 07:52:18 +0000

    • Making detaching optional.
    Some svn commands don't need to run in the background. To support this, tmsvn_bootstrap (and therefore also tmsvn) will detach the command if the _first_ argument is '-d'.

--- Support/bin/tmsvn.rb
+++ Support/bin/tmsvn.rb
@@ -4,11 +4,11 @@ require ENV['TM_SUPPORT_PATH'] + '/lib/ui'
 TextMate::UI.alert(:critical, "An SVN Bundle error occurred", "tmsvn.rb received no arguments", "OK") if ARGV.empty?
 
 script = ARGV.shift
-out,err = TextMate::Process.run(ENV['TM_RUBY'], "#{File.dirname(__FILE__)}/#{script}.rb", "--", ARGV)
+out,err = TextMate::Process.run(ENV['TM_RUBY'], "#{File.dirname(__FILE__)}/#{script}.rb", ARGV)
 
 if $? != 0
   TextMate::UI.alert(:critical, "An error occurred within the Subverion bundle", err, "OK")
   exit $?
-elsif not out.empty?
-  TextMate::UI.alert(:critical, "The SVN bundle produced output", out, "OK")
 end
+
+STDOUT << out
--- Support/bin/tmsvn_bootstrap.sh
+++ Support/bin/tmsvn_bootstrap.sh
@@ -8,4 +8,12 @@ export TM_SVN=${TM_SVN:-svn}
 require_cmd "$TM_SVN" "If you have installed svn, then you need to either <a href=\"help:anchor='search_path'%20bookID='TextMate%20Help'\">update your <tt>PATH</tt></a> or set the <tt>TM_SVN</tt> shell variable (e.g. in Preferences / Advanced)"
 bin=$(dirname "$0")
 
-$TM_RUBY "$bin/tmsvn.rb" $@ >& /dev/null &
+FIRST="$1"
+shift
+
+if [ "$FIRST" == "-d" ]
+then
+    $TM_RUBY "$bin/tmsvn.rb" $@ >& /dev/null &
+else
+    $TM_RUBY "$bin/tmsvn.rb" $FIRST $@
+fi
