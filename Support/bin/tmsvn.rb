7dbb6eb34a515c5ba5b580ac1ca2e1d7783541d1
Luke Daley <ld@ldaley.com>
2008-05-25 04:13:20 +0000

    • Call 'TextMate.exit_discard' if an error does occur.

--- Support/bin/tmsvn.rb
+++ Support/bin/tmsvn.rb
@@ -1,5 +1,6 @@
 require ENV['TM_SUPPORT_PATH'] + '/lib/tm/process'
 require ENV['TM_SUPPORT_PATH'] + '/lib/ui'
+require ENV['TM_SUPPORT_PATH'] + '/lib/exit_codes'
 
 TextMate::UI.alert(:critical, "An SVN Bundle error occurred", "tmsvn.rb received no arguments", "OK") if ARGV.empty?
 
@@ -8,7 +9,7 @@ out,err = TextMate::Process.run(ENV['TM_RUBY'], "#{File.dirname(__FILE__)}/#{scr
 
 if $? != 0 and not err.empty?
   TextMate::UI.alert(:critical, "An error occurred within the Subverion bundle", err, "OK")
-  exit $?
+  TextMate.exit_discard
 end
 
 STDOUT << out
