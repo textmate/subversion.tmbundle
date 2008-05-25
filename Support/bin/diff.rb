53f26c2cba0fd701b6db22e48a754ee3f6de00c5
Luke Daley <ld@ldaley.com>
2008-05-25 07:28:47 +0000

    • Rework all of the diff commands.
    diff.rb provides a wrapper over the DiffOperation class. It accepts a ‘revision’ argument where the '?' and ':' characters have special meaning. 
    
    When the revision arg is '?', the revision chooser is called to select one revision.
    
    When the revision arg is ':', the revision chooser is called to select two revisions (i.e. a range).
    
    By default diff.rb sends the diff to STDOUT, but if the --send-to-mate switch is set the diff is written to a temp file and opened with mate.

---
+++ Support/bin/diff.rb
@@ -0,0 +1,64 @@
+require 'optparse'
+require 'tempfile'
+require 'pathname'
+
+require "#{ENV['TM_SUPPORT_PATH']}/lib/ui"
+require "#{ENV['TM_SUPPORT_PATH']}/lib/exit_codes"
+require "#{ENV['TM_SUPPORT_PATH']}/lib/tm/process"
+require "#{ENV['TM_BUNDLE_SUPPORT']}/lib/diff_operation"
+require "#{ENV['TM_BUNDLE_SUPPORT']}/lib/revision_chooser"
+
+base = nil
+revision = nil
+send_to_mate = false
+
+optparser = OptionParser.new do |optparser|
+  optparser.banner = "Usage: #{File.basename(__FILE__)} [options] [files]"
+  optparser.separator ""
+  optparser.separator "Specific options:"
+
+  optparser.on("--base=PATH", "If present, paths will be displayed to the user relative to this") do |b|
+    base = b
+  end
+  
+  optparser.on("--send-to-mate", "If present, the diff will be sent to `mate` instead of STDOUT") do |s|
+    send_to_mate = s
+  end
+
+  optparser.on("--revision=REVISION", "") do |r|
+    revision = r
+  end
+
+end
+
+optparser.parse!
+
+files = ARGV
+
+unless files.empty?
+  base = ENV['TM_PROJECT_DIRECTORY'] || ENV['TM_DIRECTORY'] || nil if base.nil?
+    
+  if ["?", ":"].member? revision 
+    abort "only one file argument allowed with --revision=#{revision}" if files.length > 1
+    chooser = Subversion::RevisionChooser.new(files.first)
+    revision = (revision == '?') ? chooser.revision : chooser.range
+    exit 0 if revision.nil?
+  end
+    
+  diff = Subversion::DiffOperation.new(base,revision,*files).diff
+  
+  if diff.empty?
+    TextMate::UI.alert(:warning, "No differences to show", "The selected files/revisions are identical.", "OK")
+    TextMate.exit_discard
+  else
+    if send_to_mate
+      tmp = Tempfile.new(files.map{ |f| File.basename(f) }.join('-'), ENV['TMPDIR'] || '/tmp')
+      tmp.write diff
+      tmp.flush
+      out, err = TextMate::Process.run("mate", "-w", tmp.path)
+      abort err if $? != 0
+    else
+      STDOUT << diff
+    end
+  end
+end
---
+++ Support/lib/diff_operation.rb
@@ -0,0 +1,49 @@
+#!/usr/bin/env ruby -w
+
+location = File.dirname(__FILE__)
+
+require "#{ENV['TM_SUPPORT_PATH']}/lib/ui"
+require "#{ENV['TM_SUPPORT_PATH']}/lib/progress"
+require "#{location}/subversion"
+
+module Subversion
+  class DiffOperation
+
+    def initialize(base, revision, *files)
+      @base = base
+      @revision = revision
+      if @base
+        escaped_base = Regexp.escape((base =~ /\/$/) ? base : base + '/')
+        @files = files.map { |f| f.sub(/^#{escaped_base}/, '') }
+      else
+        @files = files
+      end
+    end
+    
+    def diff(quiet = false)
+      diff_op = Proc.new do
+        Dir.chdir(@base) { Subversion.run("diff", "-r", @revision, *@files) }
+      end
+      if quiet
+        diff_op.call
+      else
+        TextMate::call_with_progress(:title => "svn diff", :message => "Fetching diff (#{@revision})", &diff_op)
+      end
+    end
+
+  end
+end
+
+if __FILE__ == $0
+  STDOUT << "Enter base: "
+  base = gets.chomp
+  STDOUT << "Enter revision argument: "
+  revision = gets.chomp
+  STDOUT << "Enter description: "
+  description = gets.chomp
+  STDOUT << "Enter comma seperated file list: "
+  files = gets.chomp.split(',')
+
+  diffop = Subversion::DiffOperation.new(base,revision,description,*files)
+  STDOUT << diffop.diff
+end
