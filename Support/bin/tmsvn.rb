55c2d340aca4a1cb6a290c066ec93be5367e2669
Luke Daley <ld@ldaley.com>
2008-05-27 08:09:45 +0000

    Change execution strategies yet again…
    • Write out the output of the sub command to STDOUT regardless.
    • If the exit code was non zero, show an alert with the STDERR of the subcommand if there was any.
    • Return the exit status of the subcommand regardless.

--- Support/bin/tmsvn.rb
+++ Support/bin/tmsvn.rb
@@ -5,11 +5,8 @@ require ENV['TM_SUPPORT_PATH'] + '/lib/exit_codes'
 TextMate::UI.alert(:critical, "An SVN Bundle error occurred", "tmsvn.rb received no arguments", "OK") if ARGV.empty?
 
 script = ARGV.shift
-out,err = TextMate::Process.run(ENV['TM_RUBY'], "#{File.dirname(__FILE__)}/#{script}.rb", ARGV)
-
-if $? != 0
-  TextMate::UI.alert(:critical, "An error occurred within the Subverion bundle", err, "OK") if not err.empty?
-  TextMate.exit_discard
-end
+out, err = TextMate::Process.run(ENV['TM_RUBY'], "#{File.dirname(__FILE__)}/#{script}.rb", ARGV)
 
 STDOUT << out
+TextMate::UI.alert(:critical, "An error occurred within the Subversion bundle", err, "OK") if $? != 0 and not err.empty?
+exit $?.exitstatus
