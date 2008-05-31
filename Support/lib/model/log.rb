2668ea39d28638228b0e99efc25ea53c307677a3
Luke Daley <ld@ldaley.com>
2008-05-31 09:28:25 +0000

    • Store the log entries as Subversion::Log::Entry objects.
    This class is a Hash subclass because they need to be serialised to a plist for tm_dialog. So the point of this class is to allow entry.rev opposed to only entry['rev'].

--- Support/lib/model/log.rb
+++ Support/lib/model/log.rb
@@ -15,9 +15,9 @@ module Subversion
     def tag_start(name, attributes)
       case name
       when 'logentry'
-        @current_entry = {"rev" => attributes['revision'].to_i}
-      when 'paths'
-        @current_paths = {}
+        @current_entry = Log::Entry.new
+        @current_entry.rev = attributes['revision'].to_i
+        @current_entry.paths = {}
       when 'path'
         @current_action = attributes['action']
       end
@@ -31,10 +31,8 @@ module Subversion
         @current_entry[name] = Time.xmlschema(@tag_text)
       when 'logentry'
         @entries.push @current_entry
-      when 'paths'
-        @current_entry['paths'] = @current_paths
       when 'path'
-        @current_paths[@tag_text] = @current_action
+        @current_entry.paths[@tag_text] = @current_action
       end
     end
 
@@ -48,6 +46,22 @@ module Subversion
   end
   
   class Log
+
+    class Entry < Hash
+      def method_missing(m, *args)
+        matches = m.to_s.match(/^(.+?)(=)?$/)
+        key = matches[1]
+        is_setter = matches[2] == '='
+        if is_setter
+          return (self[key] = args.first)
+        else
+          if self.has_key? key
+            return self[key]
+          end
+        end
+        super m, *args
+      end
+    end
     attr_reader :entries
 
     def initialize(entries)
@@ -55,7 +69,7 @@ module Subversion
     end
 
     def revisions
-      @entries.collect { |entry| entry["rev"] }
+      @entries.collect { |entry| entry.rev }
     end
   end
 end
