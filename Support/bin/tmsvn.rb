2ecaeb588fba8cfdb9a4e6749fae7a9bb90bb824
Luke Daley <ld@ldaley.com>
2008-06-01 02:11:38 +0000

    • Remove tmsvn_bootstrap.sh and use tmsvn.rb directly.
    We can do this now we have require_cmd in Ruby (thanks Infininight).

--- Support/bin/tmsvn.rb
+++ Support/bin/tmsvn.rb
@@ -1,8 +1,16 @@
+#!/usr/bin/env ruby
+
 require ENV['TM_SUPPORT_PATH'] + '/lib/tm/process'
+require ENV['TM_SUPPORT_PATH'] + '/lib/textmate'
 require ENV['TM_SUPPORT_PATH'] + '/lib/ui'
 require ENV['TM_SUPPORT_PATH'] + '/lib/exit_codes'
 
 TextMate::UI.alert(:critical, "An SVN Bundle error occurred", "tmsvn.rb received no arguments", "OK") if ARGV.empty?
+
+ENV['TM_SVN'] ||= 'svn'
+ENV['TM_RUBY'] ||= 'ruby'
+
+TextMate.require_cmd(ENV['TM_SVN'], "If you have installed svn, then you need to either <a href=\"help:anchor='search_path'%20bookID='TextMate%20Help'\">update your <tt>PATH</tt></a> or set the <tt>TM_SVN</tt> shell variable (e.g. in Preferences / Advanced)")
 
 script = ARGV.shift
 out, err = TextMate::Process.run(ENV['TM_RUBY'], "#{File.dirname(__FILE__)}/#{script}.rb", ARGV)
