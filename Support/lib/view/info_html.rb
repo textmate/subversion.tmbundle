0854e530b072a6cee9884a31ad08bb68b257c5f1
Luke Daley <ld@ldaley.com>
2008-06-21 05:37:15 +0000

    • Improve the info layout (marginally).
    This still needs to be improved.

--- Support/lib/view/info_html.rb
+++ Support/lib/view/info_html.rb
@@ -11,7 +11,21 @@ module Subversion
         <div class="subversion">
           <% entries.each_with_index do |e,i| %>
             <p<%= ' class="alternate"' if ((i + 1) % 2) == 0 %>>
-              <%= htmlize e.inspect %>
+              <h3><%= e.path %> (r<%= e.revision %>)</h3>
+              <table class="blame">
+                <tr><th>URL in Repository</th></tr>
+                <tr><td style="white-space: normal;"><a href="<%= e.url %>"><%= e.url %></a></td></tr>
+              </table>
+              <h4>Last Changed…</h4>
+              <table class="blame">
+                <tr><th>Revision</th><th>Author</th><th>Date</th></tr>
+                <tr><td><%= e.commit.revision %></td><td><%= e.commit.author %></td><td><%= e.commit.date %></td></tr>
+              </table>
+              <h4>Repository…</h4>
+              <table class="blame">
+                <tr><th>URL</th><th>UUID</th></tr>
+                <tr><td><a href="<%= e.repository.root %>"><%= e.repository.root %></a></td><td><%= e.repository.uuid %></td></tr>
+              </table>
             </p>
           <% end %>
         </div>
