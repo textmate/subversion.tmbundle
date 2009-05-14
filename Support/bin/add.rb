2d5347cd3020e41d09bddde1f10efaa7eb5c25d3
Luke Daley <ld@ldaley.com>
2009-05-14 12:12:23 +0000

    • Perform the add operations relative to the project root.

--- Support/bin/add.rb
+++ Support/bin/add.rb
@@ -3,7 +3,11 @@ require "#{ENV['TM_BUNDLE_SUPPORT']}/lib/subversion"
 require "#{ENV['TM_SUPPORT_PATH']}/lib/tm/event"
 
 unless ARGV.empty?
-  out = Subversion.run("add", ARGV)
-  TextMate.event("info.scm.add.svn", "svn add", out)
+  base = Pathname.new(ENV['TM_PROJECT_DIRECTORY'] || ENV['TM_DIRECTORY'])
+  relative_args = ARGV.collect { |a| Pathname.new(a).relative_path_from(base) }
+  Dir.chdir(base) do
+    out = Subversion.run("add", relative_args)
+    TextMate.event("info.scm.add.svn", "svn add", out)
+  end
   TextMate.rescan_project
 end
