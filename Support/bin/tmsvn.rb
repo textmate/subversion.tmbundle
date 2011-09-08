51b86f81533617da36ff5939260762953571ea0e
Michael Sheets <git@whitefalls.org>
2011-09-08 01:25:29 -0500

    Remove usage of TM_RUBY

--- Support/bin/tmsvn.rb
+++ Support/bin/tmsvn.rb
@@ -8,12 +8,11 @@ require ENV['TM_SUPPORT_PATH'] + '/lib/exit_codes'
 TextMate::UI.alert(:critical, "An SVN Bundle error occurred", "tmsvn.rb received no arguments", "OK") if ARGV.empty?
 
 ENV['TM_SVN'] ||= 'svn'
-ENV['TM_RUBY'] ||= 'ruby'
 
 TextMate.require_cmd(ENV['TM_SVN'], "If you have installed svn, then you need to either <a href=\"help:anchor='search_path'%20bookID='TextMate%20Help'\">update your <tt>PATH</tt></a> or set the <tt>TM_SVN</tt> shell variable (e.g. in Preferences / Advanced)")
 
 script = ARGV.shift
-out, err = TextMate::Process.run(ENV['TM_RUBY'], "#{File.dirname(__FILE__)}/#{script}.rb", ARGV)
+out, err = TextMate::Process.run('ruby', "#{File.dirname(__FILE__)}/#{script}.rb", ARGV)
 
 STDOUT << out
 TextMate::UI.alert(:critical, "An error occurred within the Subversion bundle", err, "OK") if $? != 0 and not err.empty?
